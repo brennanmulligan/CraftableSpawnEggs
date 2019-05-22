143adb2f0369a54f584ece1ba4c5a36ec755b23e
Isaak Weidman <44002541+IsaakWeidman@users.noreply.github.com>
2019-05-22 15:54:36 -0400

    Add files via upload

--- Snake.java
+++ Snake.java
@@ -2,21 +2,65 @@ import java.util.*;
 import java.io.*;
 
 public class Snake {
-    public static void main(String[] args) throws InterruptedException{
+    public static void main(String[] args) throws InterruptedException {
+        
+        //Temporary method call to keep terminal clean as I test at home
+        clear();
+        
+        char[][] board;
 
-        Scanner test = new Scanner(System.in);
-        int frameNumber = 1;
+        Scanner keyboard = new Scanner(System.in);
+        String direction;
+        int speed;
+        boolean inValid = false;
+        
+        board = boardInit(15);
+        
+        while(inValid = false) {
+            
+            System.out.print("What direction to move '*'\n> ");
+            direction = keyboard.nextLine().toUpperCase();
 
-        //Test:
-        while(true){
-            System.out.println("Frame" + frameNumber);
-            Thread.sleep(500);
-            frameNumber++;
-            clear();
+            if (!direction.equals("LEFT") && !direction.equals("RIGHT") && !direction.equals("UP") && !direction.equals("DOWN"))
+                System.out.println("Invalid input");
         }
 
+        System.out.print("How fast do you want it to move? (enter number in miliseconds)\n> ");
+        speed = keyboard.nextInt();
+        
+        
+    }
+    
+    
+    
+    public static void print(char[][] b) {
+        
+        for(char[] ca: b) {
+            for(char c: ca)
+                System.out.printf("%c ", c);
+            System.out.println();
+        }
     }
 
+
+
+    public static char[][] boardInit(int size) {
+
+        char[][] board = new char[size][size];
+        int center = (size / 2) + 1;
+
+        for(char ca[] : board) {
+            for (char c : ca)
+            c = ' ';
+        }
+        
+        board[center][center] = '*';
+
+        return board;
+    }
+
+    
+    
     /*
      * Checks what os the program is being run on. if it is run on windows, a new command
      * is created that clears the command prompt when called. Otherwise the pre-existing
