bdd58d08602cc1a2d0400f4da2e53d0bd6af5a51
Brian Kiss <45636496+Dexter1946@users.noreply.github.com>
2019-05-23 17:21:07 -0400

    Update Snek.java

--- Snek.java
+++ Snek.java
@@ -180,4 +180,30 @@ public class Snek
 	  }
 	
 	//=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~
+	
+	/*
+	Brian Kiss	Pd. 8	5/21/2019
+	This is my own work, BK
+	Initializes 2d array grid with a buffer of null around the outside, and the inside to '0'.
+	*/
+	public static String[][] setArray (String[][] grid)
+	{
+		for (int i = 0; i < grid.length; i++)
+		{
+		    for (int j = 0; j < grid[i].length; j++)
+		    {
+		        if ( i == 0 || i == grid.length - 1)
+		            grid[i][j] = null;
+		        else
+		        {
+		            if ( j == 0 || j == grid[i].length - 1)
+		                grid[i][j] = null;
+		            else
+		                grid[i][j] = "0";
+		        }
+		    }
+		}
+		return grid;
+	}
+	//=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~
 }
