a15a46cfb4c01a87398f62e1709b19d8f5b1cda7
Brian Kiss <45636496+Dexter1946@users.noreply.github.com>
2019-05-23 17:19:02 -0400

    Update Snek.java

--- Snek.java
+++ Snek.java
@@ -7,6 +7,9 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Snek
 		implements NativeKeyListener {
 
@@ -127,6 +130,54 @@ public class Snek
 			}
 		}
 	}
+	
+	//=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~
+	
+	/*
+	Brian Kiss	Pd. 8	5/21/2019
+	This is my own work, BK
+	Method for snake that creates a 2d array for game. Allows user to choose from 3 sizes (with idiot proofing) and creates an array of '0's of that size (with added null buffer).
+	*/
+	public static String[][] selectSize()
+	  {
+		  Scanner reader = new Scanner (System.in);
+		  int size = 0;
+		  boolean invalid = true;
+		  
+		  while (invalid == true)
+		  {
+			  try 
+			  {
+				  System.out.println("Choose size of game.\n\n1\tSmall\n2\tMedium\n3\tLarge");
+				  size = reader.nextInt();
+				  invalid = false;
+			  }
+				  catch (InputMismatchException e) 
+				  {
+				    System.out.println("Invalid value!");
+				reader.next(); // this consumes the invalid token
+				continue;
+				  }
+		  }
+		  
+		  if (size == 1)	//selects different sizes based upon user answer
+		  {
+			  String[][] grid = new String[7][7];	//+2 to each array for invisible boarder on all sides
+			  return grid;
+	 	  }
+		  if (size == 2)
+		  {
+			  String[][] grid = new String[12][12];
+			  return grid;
+		  }
+		  if (size == 3)
+		  {
+			  String[][] grid = new String[17][17];
+			  return grid;
+		  }
+		  String[][] grid = new String[0][0];
+		  return grid;
+	  }
 	
 	//=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~=~
 }
